56f39dd73883bed9eb2dc84a8b201a8e7d5d7918
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Pro-2.local>
2022-08-15 13:23:21 -0500

    updated data

--- code/temp.R
+++ code/temp.R
@@ -2,12 +2,16 @@ library(tidyverse)
 library(lubridate)
 
 
-shallow_temp<- read_csv("data/temperature/shallow/temp_22-07-08.csv")
+shallow_temp<- read_csv("data/temperature/shallow/temp_shallow.csv")
 shallow_temp<- shallow_temp%>%
   mutate(datetime = as.POSIXct(datetime, format="%m/%d/%y %H:%M"))
 
-mid_temp<- read_csv("data/temperature/mid/temp_22-07-08.csv")
+mid_temp<- read_csv("data/temperature/mid/temp_mid.csv")
 mid_temp<- mid_temp%>%
+  mutate(datetime = as.POSIXct(datetime, format="%m/%d/%y %H:%M"))
+
+deep_temp<- read_csv("data/temperature/deep/temp_deep.csv")
+deep_temp<- deep_temp%>%
   mutate(datetime = as.POSIXct(datetime, format="%m/%d/%y %H:%M"))
 
 ggplot(shallow_temp)+
@@ -17,6 +21,13 @@ ggplot(shallow_temp)+
   theme_bw()+
   theme(legend.title=element_blank())
 ggplot(mid_temp)+
+  geom_line(aes(x = datetime, y = Temp_C, color = as.factor(depth)))+
+  labs(fill = ((expression("Temperature " ( degree*C)))))+
+  xlab("")+
+  theme_bw()+
+  theme(legend.title=element_blank())
+
+ggplot(deep_temp)+
   geom_line(aes(x = datetime, y = Temp_C, color = as.factor(depth)))+
   labs(fill = ((expression("Temperature " ( degree*C)))))+
   xlab("")+
